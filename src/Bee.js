20ae2b9d071c0bfbafe615245d9538a0a831399b
colin-h <colinharman@utexas.edu>
2015-06-15 13:32:29 -0500

    all tests passing

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,18 @@
 var Bee = function() {
+
+  //call the super constructor (Grub)
+  // first argument to .call is what you want this to point to
+  Grub.call(this, arguments);
+
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = "keep on growing";
+
 };
+
+// Make Bee prototype point to Grub's
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+// Bee.prototype = inherit(Grub.prototype);
 
